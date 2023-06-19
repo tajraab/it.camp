63bb68eff60f941fba9c658daca9393d813b74b5
tajraab <tajrabacevac@gmail.com>
2023-06-19 11:10:07 +0200

    dom nastavak

--- veyba/vezba17feb/DOMACI/vezba20feb/vezba2feb/domacifebruar/vezba27februar/vezba6mart/vezba8mart/vezba7april/vezba10april/vezba17aprilfunkcije/domm/app.js
+++ veyba/vezba17feb/DOMACI/vezba20feb/vezba2feb/domacifebruar/vezba27februar/vezba6mart/vezba8mart/vezba7april/vezba10april/vezba17aprilfunkcije/domm/app.js
@@ -1,7 +1,17 @@
 const inputText = document.getElementById("inputText");
 const addButon = document.getElementById("addButon");
-
+const inputContainer = document.getElementById("inputContainer");
 let vrednost = "";
+function singleCard(text) {
+  const kartica = document.createElement("div");
+  kartica.classList.add("task");
+  const hatri = document.createElement("h3");
+  hatri.innerText;
+  const checkInput = document.createElement;
+  kartica.appendChild(hatri);
+  // kartica.innerText = text;
+  return kartica;
+}
 
 console.log(inputText);
 inputText.addEventListener("input", (e) => {
@@ -12,4 +22,7 @@ addButon.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(vrednost);
   inputText.value = "";
+  inputContainer.append("vrednost");
+  console.log(singleCard(vrednost));
+  inputContainer.appendChild(singleCard(vrednost));
 });
